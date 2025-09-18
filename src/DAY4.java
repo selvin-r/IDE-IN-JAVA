9d4ff6a7d7327406fee44edac9ff4d231d9df05c
SELVIN R <steelselvin2002@gmail.com>
2025-09-18 18:58:35 +0530

    first commit

--- src/DAY4.java
+++ src/DAY4.java
@@ -131,3 +131,34 @@ class Lexicogra{
     }
 }
 
+// Anagram
+
+class Anagram {
+    public static void main(String[] args) {
+        String s = "hello";
+        String s1 = "olleh";
+
+        char [] arr = s1.toCharArray();
+
+        int count=0;
+
+        for(int i=0;i<s.length();i++){
+            for (int j=0;j<s1.length();j++){
+
+                if(s.charAt(i)==arr[j]){
+                    arr[j]='*';
+                    count++;
+                    break;
+
+                }
+            }
+        } if (s1.length()==count){
+            System.out.println("This Anagram :");
+
+        } else {
+            System.out.println("This Not Anagram :");
+        }
+
+
+    }
+}
