94213e098467d54f144b0c4907439e90e32dd935
SELVIN R <steelselvin2002@gmail.com>
2025-10-13 17:31:18 +0530

    first commit

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1334,3 +1334,65 @@ class Solutionn{
 
     }
 }
+
+
+// 69. Sqrt(x)
+//Solved
+//Easy
+//Topics
+//premium lock icon
+//Companies
+//Hint
+//Given a non-negative integer x, return the square root of x rounded down to the nearest integer.
+// The returned integer should be non-negative as well.
+//
+//You must not use any built-in exponent function or operator.
+//
+//For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+//
+//
+//Example 1:
+//
+//Input: x = 4
+//Output: 2
+//Explanation: The square root of 4 is 2, so we return 2.
+//Example 2:
+//
+//Input: x = 8
+//Output: 2
+//Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+
+
+class Sqrt {
+    public static void main(String[] args) {
+//        int testcase1 = 4;
+        int testcase2= 8;
+//        System.out.println(Root(testcase1));
+        System.out.println(Root(testcase2));
+    }
+
+    public static int Root(int x){
+        int left=1;
+        int right=x;
+        int ans=0;
+
+        while (left <= right){
+
+            int mid = left + (right-left)/2;
+
+            long m =(long) mid * mid;
+            
+            if(m==x){
+                return  mid;
+            } else if (m < x) {
+
+                ans=mid;
+                left=mid+1;
+                
+            } else {
+                right=mid-1;
+            }
+        }
+        return ans;
+    }
+}
