1e021cc38a5aa347e7d258ee01f73161f81c27c6
SELVIN R <steelselvin2002@gmail.com>
2025-10-23 14:43:00 +0530

    java code

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1568,3 +1568,61 @@ class ReverseOnlyLetters{
         return new String(arr);
     }
 }
+
+//696. Count Binary Substrings
+//Solved
+//Easy
+//Topics
+//premium lock icon
+//Companies
+//Hint
+//Given a binary string s, return the number of non-empty substrings that have the same number
+// of 0's and 1's, and all the 0's and all the 1's in these substrings are grouped consecutively.
+//
+//Substrings that occur multiple times are counted the number of times they occur.
+//
+//
+//
+//Example 1:
+//
+//Input: s = "00110011"
+//Output: 6
+//Explanation: There are 6 substrings that have equal number of consecutive
+// 1's and 0's: "0011", "01", "1100", "10", "0011", and "01".
+//Notice that some of these substrings repeat and are counted the number of times they occur.
+//Also, "00110011" is not a valid substring because all the 0's (and 1's) are not grouped together.
+//Example 2:
+//
+//Input: s = "10101"
+//Output: 4
+//Explanation: There are 4 substrings: "10", "01", "10", "01" that have equal number of consecutive 1's and 0's.
+//
+
+class CountBinary {
+    static void main() {
+//       String testcase1="00110011";
+       String testcase2="10101";
+
+//        System.out.println(Binary(testcase1));
+        System.out.println(Binary(testcase2));
+    }
+    static int Binary(String s){
+        int i=0;
+        int j=1;
+        int c=0;
+
+        for(int k=1;k<s.length();k++){
+
+            if(s.charAt(k)==s.charAt(k-1)){
+                j++;
+            } else {
+                c +=Math.min(i,j);
+                i = j;
+                j=1;
+            }
+
+        }
+        c +=Math.min(i,j);
+        return c;
+    }
+}
