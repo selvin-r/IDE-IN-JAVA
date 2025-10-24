65e01c603ea6f4cc7b7ee07b134ede63c4642599
SELVIN R <steelselvin2002@gmail.com>
2025-10-24 17:10:51 +0530

    Auth

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -1676,3 +1676,59 @@ static boolean Square(int c){
     }
     return false;
 }
+// 1365. How Many Numbers Are Smaller Than the Current Number (24-01-2025)===========
+//Solved
+//Easy
+//Topics
+//premium lock icon
+//Companies
+//Hint
+//Given the array nums, for each nums[i] find out how many numbers in the array are smaller than it. That is, for each nums[i] you have to count the number of valid j's such that j != i and nums[j] < nums[i].
+//
+//Return the answer in an array.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [8,1,2,2,3]
+//Output: [4,0,1,1,3]
+//Explanation:
+//For nums[0]=8 there exist four smaller numbers than it (1, 2, 2 and 3).
+//For nums[1]=1 does not exist any smaller number than it.
+//For nums[2]=2 there exist one smaller number than it (1).
+//For nums[3]=2 there exist one smaller number than it (1).
+//For nums[4]=3 there exist three smaller numbers than it (1, 2 and 2).
+//Example 2:
+//
+//Input: nums = [6,5,4,8]
+//Output: [2,1,0,3]
+//Example 3:
+//
+//Input: nums = [7,7,7,7]
+//Output: [0,0,0,0]
+//
+
+class Smaller {
+    static void main() {
+   int testcase1[]={8,1,2,2,3};
+        System.out.println(Many(testcase1));
+    }
+    static  int Many(int arr []) {
+
+        int ans[] = new int[arr.length];
+
+        int count = 0;
+        for (int i = 0; i < arr.length; i++) {
+            count = 0;
+            for (int j = 0; j < arr.length; j++) {
+
+                if (arr[j] < arr[i]) {
+                    count++;
+                }
+                arr[i] = count;
+            }
+        }
+        return count;
+    }
+}
