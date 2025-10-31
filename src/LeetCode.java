716da9f0556ab1eebef5fa3bf6bc7237a677d2d6
SELVIN R <steelselvin2002@gmail.com>
2025-10-31 16:40:19 +0530

    first commit

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2669,3 +2669,75 @@ class LongestConsecutiveSequence {
 
     }
 }
+
+// 3289. The Two Sneaky Numbers of Digitville  (31-10-2025)================================
+//Easy
+//Topics
+//premium lock icon
+//Companies
+//Hint
+//In the town of Digitville, there was a list of numbers called nums containing integers
+// from 0 to n - 1. Each number was supposed to appear exactly once in the list, however,
+// wo mischievous numbers sneaked in an additional time, making the list longer than usual.
+//
+//As the town detective, your task is to find these two sneaky numbers. Return an array of
+// size two containing the two numbers (in any order), so peace can return to Digitville.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [0,1,1,0]
+//
+//Output: [0,1]
+//
+//Explanation:
+//
+//The numbers 0 and 1 each appear twice in the array.
+//
+//Example 2:
+//
+//Input: nums = [0,3,2,1,3,2]
+//
+//Output: [2,3]
+//
+//Explanation:
+//
+//The numbers 2 and 3 each appear twice in the array.
+//
+//Example 3:
+//
+//Input: nums = [7,1,5,4,3,4,6,0,9,5,8,2]
+//
+//Output: [4,5]
+//
+//Explanation:
+//
+//The numbers 4 and 5 each appear twice in the array.
+//
+//
+
+class TwoSneakyNumbers {
+    static void main() {
+        int testcase1 [] = {0,1,1,0};
+        System.out.println(Arrays.toString(Digitville(testcase1)));
+    }
+    public static int[] Digitville(int[] nums){
+
+      int ans [] = new int[2];
+
+      int count=0;
+
+      for(int i=0;i<nums.length;i++){
+
+          for(int j=i+1;j< nums.length;j++){
+
+              if(nums[i]==nums[j]){
+                  ans[count++]=nums[i];
+              }
+          }
+      }
+      return  ans;
+
+    }
+}
