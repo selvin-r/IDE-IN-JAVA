75db73c50b9c92847e8a5ec1a191524747438479
SELVIN R <steelselvin2002@gmail.com>
2025-10-31 10:19:18 +0530

    first commit

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2470,3 +2470,65 @@ class KthLargestElement {
         return q.peek();
     }
 }
+
+
+// 350. Intersection of Two Arrays II
+//Solved
+//Easy
+//Topics
+//premium lock icon
+//Companies
+//Given two integer arrays nums1 and nums2, return an array of their intersection. Each element in the result must appear as many times as it shows in both arrays and you may return the result in any order.
+//
+//
+//
+//Example 1:
+//
+//Input: nums1 = [1,2,2,1], nums2 = [2,2]
+//Output: [2,2]
+//Example 2:
+//
+//Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+//Output: [4,9]
+//Explanation: [9,4] is also accepted.
+
+class Intersection {
+    static void main() {
+        int nums1[] = {1,2,2,1};
+        int nums2[]={2,2};
+
+        System.out.println(Arrays.toString(Also(nums1, nums2)));
+    }
+
+    public static int [] Also(int nums1 [] ,int nums2 []){
+
+        List<Integer> list = new ArrayList<>();
+
+
+        int i=0;
+        int j=0;
+
+        while (i<nums1.length && j<nums2.length){
+
+            if(nums1[i]==nums2[j]){
+                list.add(nums1[i]);
+                i++;
+                j++;
+            } else if (nums1[i] < nums2[j]) {
+                i++;
+
+            } else {
+                j++;
+            }
+        }
+        int [] result = new int[list.size()];
+
+        int d=0;
+
+        for (int c : list){
+            result[d++]=c;
+
+        }
+        return result;
+    }
+}
