c5dbb43e34e968f92f6baa9303117a42bd16179c
SELVIN R <steelselvin2002@gmail.com>
2025-11-01 14:47:52 +0530

    java

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -2804,3 +2804,73 @@ class  MountainArray {
 }
 
 
+// 228. Summary Ranges (01-11-2025)============================================
+//Easy
+//Topics
+//premium lock icon
+//Companies
+//You are given a sorted unique integer array nums.
+//
+//A range [a,b] is the set of all integers from a to b (inclusive).
+//
+//Return the smallest sorted list of ranges that cover all the numbers in the array exactly. That is,
+// each element of nums is covered by exactly one of the ranges, and there is no integer x such that x is in one of the
+// ranges but not in nums.
+//
+//Each range [a,b] in the list should be output as:
+//
+//"a->b" if a != b
+//"a" if a == b
+//
+//
+//Example 1:
+//
+//Input: nums = [0,1,2,4,5,7]
+//Output: ["0->2","4->5","7"]
+//Explanation: The ranges are:
+//[0,2] --> "0->2"
+//[4,5] --> "4->5"
+//[7,7] --> "7"
+//Example 2:
+//
+//Input: nums = [0,2,3,4,6,8,9]
+//Output: ["0","2->4","6","8->9"]
+//Explanation: The ranges are:
+//[0,0] --> "0"
+//[2,4] --> "2->4"
+//[6,6] --> "6"
+//[8,9] --> "8->9"
+
+
+class SummaryRanges {
+    static void main() {
+         int testcas1 [] = {0,1,2,4,5,7};
+        System.out.println(Ranges(testcas1));
+    }
+
+    public static List<String> Ranges(int [] nums) {
+        List<String> result = new ArrayList<>();
+
+        if (nums.length == 0) return result;
+
+
+        int start = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+
+            if (nums[i] != nums[i - 1] + 1) {
+                if (start == nums[i - 1]) {
+                    result.add(String.valueOf(start));
+                } else {
+                    result.add(start + "->" + nums[i - 1]);
+                }
+                start = nums[i];
+            }
+        }
+        if (start == nums[nums.length - 1]) {
+            result.add(String.valueOf(start));
+        } else {
+            result.add(start + "->" + nums[nums.length - 1]);
+        }
+        return result;
+    }
+    }
