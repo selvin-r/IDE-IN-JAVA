f1313e2181820a83908365c10f8cc8892adf6e62
SELVIN R <steelselvin2002@gmail.com>
2025-11-03 17:45:28 +0530

    java

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -3118,3 +3118,48 @@ class NumberComplement {
         return n^m;
     }
 }
+
+// 231. Power of Two  (03-11-2025)====================================================
+//Solved
+//Easy
+//Topics
+//premium lock icon
+//Companies
+//Given an integer n, return true if it is a power of two. Otherwise, return false.
+//
+//An integer n is a power of two, if there exists an integer x such that n == 2x.
+//
+//
+//
+//Example 1:
+//
+//Input: n = 1
+//Output: true
+//Explanation: 20 = 1
+//Example 2:
+//
+//Input: n = 16
+//Output: true
+//Explanation: 24 = 16
+//Example 3:
+//
+//Input: n = 3
+//Output: false
+//
+
+
+class PowerofTwo {
+    static void main() {
+        int testcase1 =1;
+        System.out.println(Power(testcase1));
+    }
+
+    public static boolean Power(int n){
+
+
+        if(n >0 && (n& (n-1))==0){
+            return true;
+        }
+        return false;
+    }
+}
