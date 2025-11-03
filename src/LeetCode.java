59ecdcf31291c1ed20eae62589791bb5fe592e08
SELVIN R <steelselvin2002@gmail.com>
2025-11-03 16:56:49 +0530

    java

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -3016,7 +3016,7 @@ class FindtheDuplicate1 {
 }
 
 
-// 1009. Complement of Base 10 Integer
+// 1009. Complement of Base 10 Integer  *(03-11-2025)===================================
 //Easy
 //Topics
 //premium lock icon
@@ -3066,5 +3066,55 @@ class Complement{
         }
 
         return n ^ m;
+    }
+}
+
+
+// 476. Number Complement  (03-11-2025)==================================
+//Solved
+//Easy
+//Topics
+//premium lock icon
+//Companies
+//The complement of an integer is the integer you get when you flip all the 0's to 1's and all the 1's to 0's in its
+// binary representation.
+//
+//For example, The integer 5 is "101" in binary and its complement is "010" which is the integer 2.
+//Given an integer num, return its complement.
+//
+//
+//
+//Example 1:
+//
+//Input: num = 5
+//Output: 2
+//Explanation: The binary representation of 5 is 101 (no leading zero bits), and its complement is 010. So you need to output 2.
+//Example 2:
+//
+//Input: num = 1
+//Output: 0
+//Explanation: The binary representation of 1 is 1 (no leading zero bits), and its complement is 0. So you need to output 0.
+//
+
+
+
+class NumberComplement {
+    static void main() {
+        int testcase1 =5;
+        System.out.println(Complement(testcase1));
+    }
+    public static int Complement(int n){
+
+        int m=0;
+        int t=n;
+
+        while (t>0){
+
+            m=(m<<1) |1;
+
+            t >>=1;
+        }
+
+        return n^m;
     }
 }
