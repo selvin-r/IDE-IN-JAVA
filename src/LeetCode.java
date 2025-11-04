152ad873f3a38c06cd355efa10801c62a9851299
SELVIN R <steelselvin2002@gmail.com>
2025-11-04 10:11:09 +0530

    java

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -3163,3 +3163,55 @@ class PowerofTwo {
         return false;
     }
 }
+
+
+// 55. Jump Game   (04-11-2025)=========================================================
+//Solved
+//Medium
+//Topics
+//premium lock icon
+//Companies
+//You are given an integer array nums. You are initially positioned at the array's first index, and each element in the
+// array represents your maximum jump length at that position.
+//
+//Return true if you can reach the last index, or false otherwise.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [2,3,1,1,4]
+//Output: true
+//Explanation: Jump 1 step from index 0 to 1, then 3 steps to the last index.
+//Example 2:
+//
+//Input: nums = [3,2,1,0,4]
+//Output: false
+//Explanation: You will always arrive at index 3 no matter what. Its maximum jump length is 0, which makes it impossible
+// to reach the last index.
+
+
+class JumpGame
+{
+    static void main()
+    {
+      int testcase1 [] = {2,3,1,1,4};
+        System.out.println(Game(testcase1));
+    }
+
+    public static  boolean Game(int nums [])
+    {
+        int point =0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(i>point)
+            {
+                return  false;
+            }
+
+            point =Math.max(point,i+nums[i]);
+
+        }
+        return true;
+    }
+}
