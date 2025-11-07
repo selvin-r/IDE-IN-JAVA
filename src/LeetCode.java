2348575e40e8b5626a9fe30a78db8dfd646b0371
SELVIN R <steelselvin2002@gmail.com>
2025-11-07 14:49:36 +0530

    java script

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -3150,7 +3150,7 @@ class NumberComplement {
 
 class PowerofTwo {
     static void main() {
-        int testcase1 =1;
+        int testcase1 =5;
         System.out.println(Power(testcase1));
     }
 
@@ -3215,6 +3215,7 @@ class JumpGame
         return true;
     }
 }
+
 
 
 // 746. Min Cost Climbing Stairs (04-11-2025)=========================================================
@@ -3330,5 +3331,81 @@ class CountingBits
             f[i]=f[i>>1]+(i&1);
         }
         return f;
+    }
+}
+
+
+// 724. Find Pivot Index
+//Solved
+//Easy
+//Topics
+//premium lock icon
+//Companies
+//Hint
+//Given an array of integers nums, calculate the pivot index of this array.
+//
+//The pivot index is the index where the sum of all the numbers strictly to the left of the index is equal to the
+// sum of all the numbers strictly to the index's right.
+//
+//If the index is on the left edge of the array, then the left sum is 0 because there are no elements to the left.
+// This also applies to the right edge of the array.
+//
+//Return the leftmost pivot index. If no such index exists, return -1.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [1,7,3,6,5,6]
+//Output: 3
+//Explanation:
+//The pivot index is 3.
+//Left sum = nums[0] + nums[1] + nums[2] = 1 + 7 + 3 = 11
+//Right sum = nums[4] + nums[5] = 5 + 6 = 11
+//Example 2:
+//
+//Input: nums = [1,2,3]
+//Output: -1
+//Explanation:
+//There is no index that satisfies the conditions in the problem statement.
+//Example 3:
+//
+//Input: nums = [2,1,-1]
+//Output: 0
+//Explanation:
+//The pivot index is 0.
+//Left sum = 0 (no elements to the left of index 0)
+//Right sum = nums[1] + nums[2] = 1 + -1 = 0
+//
+
+
+class FindPivot
+{
+    static void main()
+    {
+
+        int testcase1 []={1,7,3,6,5,6};
+        System.out.println(Find(testcase1));
+    }
+
+    public static  int Find(int arr [])
+    {
+        int sum=0;
+        for(int c : arr){
+            sum +=c;
+        }
+
+        int left=0;
+
+        for(int i=0;i< arr.length;i++){
+
+            int right =sum -left-arr[i];
+
+            if(right==left){
+                return i;
+            }
+            left +=arr[i];
+        }
+        return  -1;
     }
 }
