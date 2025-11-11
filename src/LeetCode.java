77fd897ba0e4becdf02dbf63a9c2e8c3839322cb
SELVIN R <steelselvin2002@gmail.com>
2025-11-11 13:10:52 +0530

    first commit

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -3660,3 +3660,78 @@ class EvaluateReverse
        return  arr[0];
     }
 }
+
+
+// 35. Search Insert Position    (11-11-2025)========================================
+//Solved
+//Easy
+//Topics
+//premium lock icon
+//Companies
+//Given a sorted array of distinct integers and a target value, return the index if the target is found. If not,
+// return the index where it would be if it were inserted in order.
+//
+//You must write an algorithm with O(log n) runtime complexity.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [1,3,5,6], target = 5
+//Output: 2
+//Example 2:
+//
+//Input: nums = [1,3,5,6], target = 2
+//Output: 1
+//Example 3:
+//
+//Input: nums = [1,3,5,6], target = 7
+//Output: 4
+//
+//
+//Constraints:
+//
+//1 <= nums.length <= 104
+//-104 <= nums[i] <= 104
+//nums contains distinct values sorted in ascending order.
+//-104 <= target <= 104
+
+
+class SearchInsert
+{
+    static void main()
+    {
+
+        int testcase1 [] ={1,3,5,6};
+        int target = 5;
+        System.out.println(Insert(testcase1,target));
+
+    }
+
+   public static  int Insert(int arr [] ,int n)
+    {
+      int left=0;
+      int right=arr.length-1;
+
+      while (left<right)
+      {
+          int mid = (left+right)/2;
+
+
+          if(arr[mid]==n)
+          {
+              return  mid;
+          }
+
+          if(arr[mid] < n)
+          {
+              left =mid+1;
+          }
+           else
+          {
+              right=mid-1;
+          }
+      }
+      return left;
+    }
+}
