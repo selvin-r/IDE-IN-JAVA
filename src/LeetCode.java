69dddcbf96e34b296fad1ec21f6147a4faaa0a0d
SELVIN R <steelselvin2002@gmail.com>
2025-11-22 19:33:30 +0530

    first commit

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -4415,3 +4415,67 @@ class MultiplyingFoundValues {
         return original;
     }
 }
+
+// 3190. Find Minimum Operations to Make All Elements Divisible by Three  (22-11-2025)======================
+//Easy
+//Topics
+//premium lock icon
+//Companies
+//Hint
+//You are given an integer array nums. In one operation, you can add or subtract 1 from any element of nums.
+//
+//Return the minimum number of operations to make all elements of nums divisible by 3.
+//
+//
+//
+//Example 1:
+//
+//Input: nums = [1,2,3,4]
+//
+//Output: 3
+//
+//Explanation:
+//
+//All array elements can be made divisible by 3 using 3 operations:
+//
+//Subtract 1 from 1.
+//Add 1 to 2.
+//Subtract 1 from 4.
+//Example 2:
+//
+//Input: nums = [3,6,9]
+//
+//Output: 0
+//
+//
+//
+//Constraints:
+//
+//1 <= nums.length <= 50
+//1 <= nums[i] <= 50
+
+
+class FindMinimumOperations{
+
+    static void main() {
+
+        int nums []={1,2,3,4};
+        System.out.println(Operations(nums));
+    }
+
+    public static  int Operations(int nums []){
+
+        int count=0;
+
+
+        for(int i=0;i<nums.length;i++){
+
+            if(nums[i]%3!=0){
+
+                nums[i]=nums[i]+1;
+                count++;
+            }
+        }
+        return count;
+    }
+}
