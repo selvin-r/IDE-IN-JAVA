2ea1ba2988bade353ea0d201670a960d8ab64012
SELVIN R <steelselvin2002@gmail.com>
2025-12-02 12:02:34 +0530

    first commit

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -4845,3 +4845,113 @@ class RepeatedSubstring {
     }
 
 }
+
+// 500. Keyboard Row  (02-12-2025)================================================================
+//Easy
+//Topics
+//premium lock icon
+//Companies
+//Given an array of strings words, return the words that can be typed using letters of the alphabet on only one row of
+// American keyboard like the image below.
+//
+//Note that the strings are case-insensitive, both lowercased and uppercased of the same letter are treated as if they
+// are at the same row.
+//
+//In the American keyboard:
+//
+//the first row consists of the characters "qwertyuiop",
+//the second row consists of the characters "asdfghjkl", and
+//the third row consists of the characters "zxcvbnm".
+//
+//
+//
+//Example 1:
+//
+//Input: words = ["Hello","Alaska","Dad","Peace"]
+//
+//Output: ["Alaska","Dad"]
+//
+//Explanation:
+//
+//Both "a" and "A" are in the 2nd row of the American keyboard due to case insensitivity.
+//
+//Example 2:
+//
+//Input: words = ["omk"]
+//
+//Output: []
+//
+//Example 3:
+//
+//Input: words = ["adsdf","sfd"]
+//
+//Output: ["adsdf","sfd"]
+//
+//
+//
+//Constraints:
+//
+//1 <= words.length <= 20
+//1 <= words[i].length <= 100
+//words[i] consists of English letters (both lowercase and uppercase).
+
+
+
+class KeyboardRow {
+    static void main() {
+        String words [] ={"Hello","Alaska","Dad","Peace"};
+        System.out.println(Arrays.toString(Row(words)));
+    }
+    static String [] Row(String [] words){
+
+
+        String row1 ="qwertyuiop";
+        String row2 ="asdfghjkl";
+        String row3 ="zxcvbnm";
+
+        List<String>  result = new ArrayList<>();
+
+
+        for(String word :words){
+            String w =word.toLowerCase();
+
+
+
+            int row=0;
+
+            char First =w.charAt(0);
+
+
+
+            if(row1.indexOf(First) !=-1){
+                row =1;
+
+            } else if(row2.indexOf(First) !=-1){
+                row=2;
+            } else if (row3.indexOf(First) !=-1) {
+
+                row =3;
+            }
+           boolean ok = true;
+            for(int i=1;i<w.length();i++){
+                char ch =w.charAt(i);
+
+
+                if(row ==1 && row1.indexOf(ch)==-1){
+                    ok=false;
+                    break;
+                } else if (row==2 && row2.indexOf(ch)==-1){
+                    ok=false;
+                    break;
+                } else if (row==3 && row3.indexOf(ch)==-1){
+                    ok=false;
+                    break;
+                }
+            }
+            if(ok){
+                result.add(word);
+            }
+        }
+        return result.toArray(new String[0]);
+    }
+}
