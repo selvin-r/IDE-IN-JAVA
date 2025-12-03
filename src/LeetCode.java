49a1e22e39b6c797d7a130d6516533d55a1ecf56
SELVIN R <steelselvin2002@gmail.com>
2025-12-03 16:21:02 +0530

    first commit

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -5026,29 +5026,145 @@ class ValidSudoku {
         System.out.println(Sudoku(input));
     }
 
-    static boolean Sudoku(char [][] b) {
+    static boolean Sudoku(char[][] b) {
 
 
         Set s = new HashSet();
 
 
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
 
-                char c =b[i][j];
+                char c = b[i][j];
 
 
-
-                if(c=='.'){
+                if (c == '.') {
                     continue;
                 }
 
-                if(!s.add(c+"In Row"+i) || !s.add(c+ "In Col"+j) || !s.add(c+ "In block"+i/3+'.'+j/3)){
+                if (!s.add(c + "In Row" + i) || !s.add(c + "In Col" + j) || !s.add(c + "In block" + i / 3 + '.' + j / 3)) {
                     return false;
                 }
 
             }
         }
-        return  true;
+        return true;
+    }
+}
+
+// 37. Sudoku Solver    (03-12-2025)===================================================================================
+//Hard
+//Topics
+//premium lock icon
+//Companies
+//Hint
+//Write a program to solve a Sudoku puzzle by filling the empty cells.
+//
+//A sudoku solution must satisfy all of the following rules:
+//
+//Each of the digits 1-9 must occur exactly once in each row.
+//Each of the digits 1-9 must occur exactly once in each column.
+//Each of the digits 1-9 must occur exactly once in each of the 9 3x3 sub-boxes of the grid.
+//The '.' character indicates empty cells.
+//
+//
+//
+//Example 1:
+//
+//
+//Input: board = [["5","3",".",".","7",".",".",".","."],
+// ["6",".",".","1","9","5",".",".","."],
+// [".","9","8",".",".",".",".","6","."],
+// ["8",".",".",".","6",".",".",".","3"],
+// ["4",".",".","8",".","3",".",".","1"],
+// ["7",".",".",".","2",".",".",".","6"],
+// [".","6",".",".",".",".","2","8","."],
+// [".",".",".","4","1","9",".",".","5"],
+// [".",".",".",".","8",".",".","7","9"]]
+//Output: [["5","3","4","6","7","8","9","1","2"],
+// ["6","7","2","1","9","5","3","4","8"],
+// ["1","9","8","3","4","2","5","6","7"],
+// ["8","5","9","7","6","1","4","2","3"],
+// ["4","2","6","8","5","3","7","9","1"],
+// ["7","1","3","9","2","4","8","5","6"],
+// ["9","6","1","5","3","7","2","8","4"],
+// ["2","8","7","4","1","9","6","3","5"],
+// ["3","4","5","2","8","6","1","7","9"]]
+//Explanation: The input board is shown above and the only valid solution is shown below:
+//
+//
+//
+//
+//Constraints:
+//
+//board.length == 9
+//board[i].length == 9
+//board[i][j] is a digit or '.'.
+//It is guaranteed that the input board has only one solution.
+
+
+
+class SudokuSolver{
+    static void main() {
+
+        char[][] board = {
+                {'5','3','.','.','7','.','.','.','.'},
+                {'6','.','.','1','9','5','.','.','.'},
+                {'.','9','8','.','.','.','.','6','.'},
+                {'8','.','.','.','6','.','.','.','3'},
+                {'4','.','.','8','.','3','.','.','1'},
+                {'7','.','.','.','2','.','.','.','6'},
+                {'.','6','.','.','.','.','2','8','.'},
+                {'.','.','.','4','1','9','.','.','5'},
+                {'.','.','.','.','8','.','.','7','9'}
+        };
+        System.out.println(Solve(board));
+
+    }
+    public static  boolean Solve(char [] [] b){
+
+
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+
+                if(b[i][j]=='.'){
+
+
+                    for(char c='1';c<='9';c++){
+
+
+                        if(isValid(b,i,j,c)){
+                            b[i][j]=c;
+
+                            if(Solve(b)){
+                                return true;
+                            } else {
+                                b[i][j]='.';
+                            }
+                        }
+
+                    }
+                    return false;
+                }
+            }
+
+        }
+        return true;
+    }
+
+    static boolean isValid(char b [][] ,int row,int col,char c){
+
+
+        for (int i = 0; i < 9; i++) {
+            if (b[i][col] == c)
+                return false;
+
+            if (b[row][i] == c)
+                return false;
+
+            if (b[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c)
+                return false;
+        }
+        return true;
     }
 }
