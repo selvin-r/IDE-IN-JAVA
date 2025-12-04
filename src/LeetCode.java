17967b47e8a38d3d47284073f7d9798322e858da
SELVIN R <steelselvin2002@gmail.com>
2025-12-04 14:35:09 +0530

    first commit

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -5103,43 +5103,43 @@ class ValidSudoku {
 //It is guaranteed that the input board has only one solution.
 
 
-
-class SudokuSolver{
+class SudokuSolver {
     static void main() {
 
         char[][] board = {
-                {'5','3','.','.','7','.','.','.','.'},
-                {'6','.','.','1','9','5','.','.','.'},
-                {'.','9','8','.','.','.','.','6','.'},
-                {'8','.','.','.','6','.','.','.','3'},
-                {'4','.','.','8','.','3','.','.','1'},
-                {'7','.','.','.','2','.','.','.','6'},
-                {'.','6','.','.','.','.','2','8','.'},
-                {'.','.','.','4','1','9','.','.','5'},
-                {'.','.','.','.','8','.','.','7','9'}
+                {'5', '3', '.', '.', '7', '.', '.', '.', '.'},
+                {'6', '.', '.', '1', '9', '5', '.', '.', '.'},
+                {'.', '9', '8', '.', '.', '.', '.', '6', '.'},
+                {'8', '.', '.', '.', '6', '.', '.', '.', '3'},
+                {'4', '.', '.', '8', '.', '3', '.', '.', '1'},
+                {'7', '.', '.', '.', '2', '.', '.', '.', '6'},
+                {'.', '6', '.', '.', '.', '.', '2', '8', '.'},
+                {'.', '.', '.', '4', '1', '9', '.', '.', '5'},
+                {'.', '.', '.', '.', '8', '.', '.', '7', '9'}
         };
         System.out.println(Solve(board));
 
     }
-    public static  boolean Solve(char [] [] b){
+
+    public static boolean Solve(char[][] b) {
 
 
-        for(int i=0;i<9;i++){
-            for(int j=0;j<9;j++){
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
 
-                if(b[i][j]=='.'){
-
-
-                    for(char c='1';c<='9';c++){
+                if (b[i][j] == '.') {
 
 
-                        if(isValid(b,i,j,c)){
-                            b[i][j]=c;
+                    for (char c = '1'; c <= '9'; c++) {
 
-                            if(Solve(b)){
+
+                        if (isValid(b, i, j, c)) {
+                            b[i][j] = c;
+
+                            if (Solve(b)) {
                                 return true;
                             } else {
-                                b[i][j]='.';
+                                b[i][j] = '.';
                             }
                         }
 
@@ -5152,7 +5152,7 @@ class SudokuSolver{
         return true;
     }
 
-    static boolean isValid(char b [][] ,int row,int col,char c){
+    static boolean isValid(char b[][], int row, int col, char c) {
 
 
         for (int i = 0; i < 9; i++) {
@@ -5166,5 +5166,85 @@ class SudokuSolver{
                 return false;
         }
         return true;
+    }
+}
+
+// 43. Multiply Strings  (04.12.2025)=========================================================================
+//Medium
+//Topics
+//premium lock icon
+//Companies
+//Given two non-negative integers num1 and num2 represented as strings, return the product of num1 and num2,
+// also represented as a string.
+//
+//Note: You must not use any built-in BigInteger library or convert the inputs to integer directly.
+//
+//
+//
+//Example 1:
+//
+//Input: num1 = "2", num2 = "3"
+//Output: "6"
+//Example 2:
+//
+//Input: num1 = "123", num2 = "456"
+//Output: "56088"
+//
+//
+//Constraints:
+//
+//1 <= num1.length, num2.length <= 200
+//num1 and num2 consist of digits only.
+//Both num1 and num2 do not contain any leading zero, except the number 0 itself.
+
+
+class Multiply {
+    static void main() {
+
+        String num1 = "123456789";
+        String num2 = "987654321";
+        System.out.println(Digits(num1, num2));
+    }
+
+    static String Digits(String num1, String num2) {
+
+
+        int m = num1.length();
+        int n = num2.length();
+
+
+        int[] result = new int[m + n];
+
+
+        for (int i = m - 1; i >= 0; i--) {
+
+            int d1 = num1.charAt(i) - '0';
+
+
+            for (int j = n - 1; j >= 0; j--) {
+                int d2 = num2.charAt(j) - '0';
+
+
+                int mul = d1 * d2;
+
+                int sum = mul + result[i + j + 1];
+
+                result[i + j + 1] = sum % 10;
+                result[i + j] += sum / 10;
+
+
+            }
+
+        }
+
+        StringBuffer sb = new StringBuffer();
+
+        for (int num : result) {
+
+            if (!(sb.length() == 0 && num == 0)) {
+                sb.append(num);
+            }
+        }
+        return sb.length() == 0 ? "0" : sb.toString();
     }
 }
