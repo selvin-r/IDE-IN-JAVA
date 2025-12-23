2f3eee5d39a04840f48818b1e619da50fe096291
SELVIN R <steelselvin2002@gmail.com>
2025-12-23 12:10:27 +0530

    password

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -5631,9 +5631,9 @@ class RotateArray {
 
 
 
-//     if(k>nums.length){
-//         k=k%nums.length;
-//     }
+     if(k>nums.length){
+         k=k%nums.length;
+     }
      int left=0;
      int right=nums.length-1-k;
      int nextleft=right+1;
@@ -5678,11 +5678,92 @@ class RotateArray {
 
 }
 
+// 2299. Strong Password Checker II   (23-12-2025)==================================
+//Solved
+//Easy
+//Topics
+//premium lock icon
+//Companies
+//Hint
+//A password is said to be strong if it satisfies all the following criteria:
+//
+//It has at least 8 characters.
+//It contains at least one lowercase letter.
+//It contains at least one uppercase letter.
+//It contains at least one digit.
+//It contains at least one special character. The special characters are the characters in the following string: "!@#$%^&*()-+".
+//It does not contain 2 of the same character in adjacent positions (i.e., "aab" violates this condition, but "aba" does not).
+//Given a string password, return true if it is a strong password. Otherwise, return false.
+//
+//
+//
+//Example 1:
+//
+//Input: password = "IloveLe3tcode!"
+//Output: true
+//Explanation: The password meets all the requirements. Therefore, we return true.
+//Example 2:
+//
+//Input: password = "Me+You--IsMyDream"
+//Output: false
+//Explanation: The password does not contain a digit and also contains 2 of the same character in adjacent positions. Therefore, we return false.
+//Example 3:
+//
+//Input: password = "1aB!"
+//Output: false
+//Explanation: The password does not meet the length requirement. Therefore, we return false.
+//
+//
+//Constraints:
+//
+//1 <= password.length <= 100
+//password consists of letters, digits, and special characters: "!@#$%^&*()-+".
 
 
+class StrongPassword {
+    static void main() {
+        String password ="IloveLe3tcode!";
+        System.out.println(Password(password));
+    }
+
+    static boolean Password(String pass) {
+
+        if(pass.length()<8){
+            return false;
+        }
+
+        boolean hasupper=false;
+        boolean haslower=false;
+        boolean hasnumber=false;
+        boolean hasspecial=false;
+
+        String specialchar="!@$%^&*()-+";
 
 
+        for(int i=0;i<pass.length();i++){
 
+
+            char c =pass.charAt(i);
+
+
+            if(i>0 && pass.charAt(i)==pass.charAt(i-1)){
+                return false;
+            }
+
+            if(Character.isUpperCase(c)){
+                hasupper=true;
+            } else if (Character.isLowerCase(c)){
+                haslower=true;
+            } else if (Character.isDigit(c)){
+                hasnumber=true;
+            } else if (specialchar.indexOf(c)!=-1){
+                hasspecial=true;
+            }
+        }
+
+        return  hasupper&&haslower&&hasnumber&&hasspecial;
+    }
+}
 
 
 
