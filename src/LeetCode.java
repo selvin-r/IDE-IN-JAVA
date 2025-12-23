abc2b41b6ec0e78adda97635da29e5eb5b9b9ffd
SELVIN R <steelselvin2002@gmail.com>
2025-12-23 14:13:21 +0530

    first commit

--- src/LeetCode.java
+++ src/LeetCode.java
@@ -5766,10 +5766,72 @@ class StrongPassword {
 }
 
 
+// 49. Group Anagrams   (23-12-25)====================================
+//Solved
+//Medium
+//Topics
+//premium lock icon
+//Companies
+//Given an array of strings strs, group the anagrams together. You can return the answer in any order.
+//
+//
+//
+//Example 1:
+//
+//Input: strs = ["eat","tea","tan","ate","nat","bat"]
+//
+//Output: [["bat"],["nat","tan"],["ate","eat","tea"]]
+//
+//Explanation:
+//
+//There is no string in strs that can be rearranged to form "bat".
+//The strings "nat" and "tan" are anagrams as they can be rearranged to form each other.
+//The strings "ate", "eat", and "tea" are anagrams as they can be rearranged to form each other.
+//Example 2:
+//
+//Input: strs = [""]
+//
+//Output: [[""]]
+//
+//Example 3:
+//
+//Input: strs = ["a"]
+//
+//Output: [["a"]]
+//
+//
+//
+//Constraints:
+//
+//1 <= strs.length <= 104
+//0 <= strs[i].length <= 100
+//strs[i] consists of lowercase English letters.
 
 
+class GroupAnagrams {
+    static void main() {
+       String arr [] ={"eat","tea","tan","ate","nat","bat"};
+        System.out.println(Anagrams(arr));
+    }
+    static List<List<String>> Anagrams(String [] strs){
+
+        HashMap<String, List<String>> map = new HashMap<>();
+
+        for (String s : strs) {
+
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
 
 
+            String key = new String(arr);
+
+            map.putIfAbsent(key, new ArrayList<>());
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
 
 
 
