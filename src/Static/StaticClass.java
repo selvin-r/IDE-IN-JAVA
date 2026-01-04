8dd54ddaf2831c7dfb5573756738a999c2468040
SELVIN R <steelselvin2002@gmail.com>
2026-01-04 21:32:04 +0530

    first commit

--- src/Static/StaticClass.java
+++ src/Static/StaticClass.java
@@ -36,3 +36,286 @@ public class StaticClass {
 
     }
 }
+
+// Demo
+
+
+package Static;
+
+public class StaticClass {
+
+
+    StaticClass() {
+        System.out.println("Inside Constructor");
+    }
+
+
+    static void main() {
+
+        System.out.println("Inside main Mehods");
+        new StaticClass();
+        new StaticClass();
+    }
+
+    {
+        System.out.println("Inside Non Static Block");
+    }
+    {
+        System.out.println("Inside Non Static Block");
+    }
+
+
+    static {
+        System.out.println("\nInside Static 1");
+    }
+
+    static void m() {
+        System.out.println("\nInside m Mehod ");
+    }
+
+    static {
+        System.out.println("\nInside Static 2");
+
+    }
+}
+
+
+package Static;
+
+public class StaticClass {
+
+
+    StaticClass() {
+        System.out.println("Inside Constructor");
+    }
+
+
+    static void main() {
+
+        System.out.println("Inside main Mehods");
+        new StaticClass();
+        new StaticClass();
+    }
+
+    {
+        System.out.println("Inside Non Static Block");
+    }
+    {
+        System.out.println("Inside Non Static Block");
+    }
+
+
+    static {
+        System.out.println("\nInside Static 1");
+    }
+
+    static void m() {
+        System.out.println("\nInside m Mehod ");
+    }
+
+    static {
+        System.out.println("\nInside Static 2");
+
+    }
+}
+
+
+
+package Static;
+
+public class StaticClass {
+
+
+    StaticClass() {
+        System.out.println("Inside Constructor");
+    }
+
+
+    static void main() {
+
+        System.out.println("Inside main Mehods");
+        new StaticClass();
+        new StaticClass();
+    }
+
+    {
+        System.out.println("Inside Non Static Block");
+    }
+    {
+        System.out.println("Inside Non Static Block");
+    }
+
+
+    static {
+        System.out.println("\nInside Static 1");
+    }
+
+    static void m() {
+        System.out.println("\nInside m Mehod ");
+    }
+
+    static {
+        System.out.println("\nInside Static 2");
+
+    }
+}
+
+
+package Static;
+
+public class StaticClass {
+
+
+    StaticClass() {
+        System.out.println("Inside Constructor");
+    }
+
+
+    static void main() {
+
+        System.out.println("Inside main Mehods");
+        new StaticClass();
+        new StaticClass();
+    }
+
+    {
+        System.out.println("Inside Non Static Block");
+    }
+    {
+        System.out.println("Inside Non Static Block");
+    }
+
+
+    static {
+        System.out.println("\nInside Static 1");
+    }
+
+    static void m() {
+        System.out.println("\nInside m Mehod ");
+    }
+
+    static {
+        System.out.println("\nInside Static 2");
+
+    }
+}
+
+
+package Static;
+
+public class StaticClass {
+
+
+    StaticClass() {
+        System.out.println("Inside Constructor");
+    }
+
+
+    static void main() {
+
+        System.out.println("Inside main Mehods");
+        new StaticClass();
+        new StaticClass();
+    }
+
+    {
+        System.out.println("Inside Non Static Block");
+    }
+    {
+        System.out.println("Inside Non Static Block");
+    }
+
+
+    static {
+        System.out.println("\nInside Static 1");
+    }
+
+    static void m() {
+        System.out.println("\nInside m Mehod ");
+    }
+
+    static {
+        System.out.println("\nInside Static 2");
+
+    }
+}
+
+
+package Static;
+
+public class StaticClass {
+
+
+    StaticClass() {
+        System.out.println("Inside Constructor");
+    }
+
+
+    static void main() {
+
+        System.out.println("Inside main Mehods");
+        new StaticClass();
+        new StaticClass();
+    }
+
+    {
+        System.out.println("Inside Non Static Block");
+    }
+    {
+        System.out.println("Inside Non Static Block");
+    }
+
+
+    static {
+        System.out.println("\nInside Static 1");
+    }
+
+    static void m() {
+        System.out.println("\nInside m Mehod ");
+    }
+
+    static {
+        System.out.println("\nInside Static 2");
+
+    }
+}
+
+
+package Static;
+
+public class StaticClass {
+
+
+    StaticClass() {
+        System.out.println("Inside Constructor");
+    }
+
+
+    static void main() {
+
+        System.out.println("Inside main Mehods");
+        new StaticClass();
+        new StaticClass();
+    }
+
+    {
+        System.out.println("Inside Non Static Block");
+    }
+    {
+        System.out.println("Inside Non Static Block");
+    }
+
+
+    static {
+        System.out.println("\nInside Static 1");
+    }
+
+    static void m() {
+        System.out.println("\nInside m Mehod ");
+    }
+
+    static {
+        System.out.println("\nInside Static 2");
+
+    }
+}
